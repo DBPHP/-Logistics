90d546ee39d32531e4629c36c56196aa0cd9b614
N551JB\陳永平 <milk19961014@gmail.com>
2017-06-23 01:03:58 +0800

    update from https://github.com/yougchen/-Logistics

--- actionlogin.php
+++ actionlogin.php
@@ -24,6 +24,7 @@
 
 <h4>
 <?php
+ob_start();
 session_start();
 include("config.php");
 
@@ -38,15 +39,21 @@ $row = mysqli_fetch_assoc($result);
 if ($total_records > 0) {
     if($row["manager_right"] == 0) {
        $_SESSION["loginsession"] = $account;
-       header("Location:function.php");
+       session_write_close();
+       header("Location:function.php");die();
+        ob_end_flush(); 
     } else {
         $_SESSION["loginsession"] = $account;
-        header("Location:manager.php");
+        session_write_close();
+        header("Location:manager.php");die();
+         ob_end_flush(); 
     }
 } else {
+    header("refresh:3;url=login.php");
+    ob_end_flush();    
     echo "登入失敗!!!";
-    header("refresh:3;url = login.php");
 }
 ?>
 <h4/>
 <html/>
+
